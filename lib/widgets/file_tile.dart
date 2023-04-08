8be3fee62d6f0febd4eb0b19584ce950c5ebac7f
gecko10000 <levtey@gmail.com>
2023-04-08 13:30:09 -0700

    Remove unused code

--- lib/widgets/file_tile.dart
+++ lib/widgets/file_tile.dart
@@ -1,21 +1,10 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:tagit_frontend/misc/colors.dart';
 import 'package:tagit_frontend/objects/saved_file.dart';
 
 class FileTile extends StatelessWidget {
-  FileTile({super.key, required this.file}) {
-    random = Random(file.name.hashCode);
-  }
+  const FileTile({super.key, required this.file});
 
   final SavedFile file;
-
-  late final Random random;
-
-  int _color() {
-    return random.nextInt(255);
-  }
 
   @override
   Widget build(BuildContext context) {
