da6112afdf70bc0f9109f7c6a305d3461d9e53ac
gecko10000 <levtey@gmail.com>
2023-04-21 17:48:34 -0700

    Respond to enter presses on renaming and deletion

--- lib/objects/common.dart
+++ lib/objects/common.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tagit_frontend/misc/strings.dart';
 
-void renameObject<T>(BuildContext context, String objectType, String objectName, T object,
-    Future<void> Function(T, String) renamingFunction, void Function()? refreshCallback, TextEditingController controller) {
+void renameObject<T>(
+    BuildContext context,
+    String objectType,
+    String objectName,
+    T object,
+    Future<void> Function(T, String) renamingFunction,
+    void Function()? refreshCallback,
+    TextEditingController controller) {
   Future<String?> newName = showDialog<String>(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text("Renaming ${objectType.toTitleCase()} \"$objectName\""),
-        content: Stack(
-          children: [
-            TextField(
-              controller: controller,
-              autofocus: true,
-              autocorrect: false,
+            title: Text("Renaming ${objectType.toTitleCase()} \"$objectName\""),
+            content: Stack(
+              children: [
+                TextField(
+                  onSubmitted: ((name) => Navigator.pop(context, name)),
+                  controller: controller,
+                  autofocus: true,
+                  autocorrect: false,
+                ),
+              ],
             ),
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context, null),
-            child: const Text("Cancel"),
-          ),
-          TextButton(
-            onPressed: () => Navigator.pop(context, controller.value.text),
-            child: const Text("Rename"),
-          ),
-        ],
-      ));
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, null),
+                child: const Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, controller.value.text),
+                child: const Text("Rename"),
+              ),
+            ],
+          ));
   newName.then((value) async {
     if (value == null) return;
     await renamingFunction(object, value);
@@ -34,22 +42,38 @@ void renameObject<T>(BuildContext context, String objectType, String objectName,
   });
 }
 
-void deleteObject<T>(BuildContext context, String objectType, String objectName, T object, Future<void> Function(T) deletionFunction, void Function()? refreshCallback) {
+void deleteObject<T>(
+    BuildContext context,
+    String objectType,
+    String objectName,
+    T object,
+    Future<void> Function(T) deletionFunction,
+    void Function()? refreshCallback) {
   Future<bool?> deleted = showDialog<bool>(
     context: context,
-    builder: (context) => AlertDialog(
-      title: Text("Delete ${objectType.toTitleCase()}"),
-      content: Text("Are you sure you want to delete ${objectType.toLowerCase()} \"$objectName\"?"),
-      actions: [
-        TextButton(
-          child: const Text("Cancel"),
-          onPressed: () => Navigator.pop(context, false),
-        ),
-        TextButton(
-          child: const Text("Delete", style: TextStyle(color: Colors.red)),
-          onPressed: () => Navigator.pop(context, true),
-        ),
-      ],
+    builder: (context) => RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) {
+        if (event.logicalKey == LogicalKeyboardKey.enter) {
+          Navigator.pop(context, true);
+        }
+      },
+      child: AlertDialog(
+        title: Text("Delete ${objectType.toTitleCase()}"),
+        content: Text(
+            "Are you sure you want to delete ${objectType.toLowerCase()} \"$objectName\"?"),
+        actions: [
+          TextButton(
+            child: const Text("Cancel"),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          TextButton(
+            child: const Text("Delete", style: TextStyle(color: Colors.red)),
+            onPressed: () => Navigator.pop(context, true),
+          ),
+        ],
+      ),
     ),
   );
   deleted.then((value) async {
