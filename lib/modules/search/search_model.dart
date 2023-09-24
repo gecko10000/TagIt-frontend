47c53ad9d0cbaa74991b5cceb4bdbc68ffd35bea
gecko10000 <levtey@gmail.com>
2023-09-24 14:53:48 -0700

    Make search provider autoDispose

--- lib/modules/search/search_model.dart
+++ lib/modules/search/search_model.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tagit_frontend/model/api/search.dart';
 import 'package:tagit_frontend/model/object/saved_file.dart';
+import 'package:tagit_frontend/modules/search/help.dart';
 
-final searchInputProvider = StateProvider((ref) => "");
+final searchInputProvider = StateProvider.autoDispose((ref) => "");
 
-final searchResultsProvider = FutureProvider<List<SavedFileState>>((ref) async {
+final searchResultsProvider =
+    FutureProvider.autoDispose<List<SavedFileState>>((ref) async {
   String query = ref.watch(searchInputProvider);
   return SearchAPI.fileSearch(query);
 });
+
+void openSearchHelpPage(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => const SearchHelpPage()));
+}
