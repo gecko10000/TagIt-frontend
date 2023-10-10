00342241115fc7b2bf0908bdc93a6746d59fa511
gecko10000 <levtey@gmail.com>
2023-10-10 16:46:27 -0700

    Ensure platform is not web when sharing, catch any exceptions

--- lib/modules/management/file/share.dart
+++ lib/modules/management/file/share.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:tagit_frontend/common/extension/build_context.dart';
@@ -9,11 +10,17 @@ import 'package:tagit_frontend/model/object/saved_file.dart';
 
 void shareFile(BuildContext context, SavedFileState savedFile) async {
   // https://pub.dev/packages/share_plus#platform-support
-  if (Platform.isWindows || Platform.isLinux) {
+  // note: short circuit with web because the boolean getters don't work on web.
+  if (kIsWeb || Platform.isWindows || Platform.isLinux) {
     context.showTextSnackBar("Sharing is not supported on this platform.");
   }
   final tempDir = await getTemporaryDirectory();
   final path = tempDir.uri.resolve(savedFile.name).path;
   await FileAPI.downloadFile(savedFile, path);
-  Share.shareXFiles([XFile(path)]);
+  try {
+    Share.shareXFiles([XFile(path)]);
+  } catch (ex) {
+    if (!context.mounted) return;
+    context.showTextSnackBar("Sharing failed: $ex");
+  }
 }
