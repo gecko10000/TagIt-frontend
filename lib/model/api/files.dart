62c69364d4d8c55e4ade10e15b1813f4426c2150
gecko10000 <levtey@gmail.com>
2023-11-01 09:52:11 -0700

    Format code and remove fade-in

--- lib/model/api/files.dart
+++ lib/model/api/files.dart
@@ -18,8 +18,8 @@ class FileAPI {
   // no constructor
   FileAPI._();
 
-  static Future<List<SavedFileState>> getAllFiles(FileOrder order,
-      bool reversed) async {
+  static Future<List<SavedFileState>> getAllFiles(
+      FileOrder order, bool reversed) async {
     final response = await client.get("/files/all",
         options: Options(headers: {
           "fileOrder": order.name,
@@ -34,9 +34,7 @@ class FileAPI {
       final path = pFile.path;
       if (path == null) return null;
       final file = File(path);
-      return file
-          .lastModifiedSync()
-          .millisecondsSinceEpoch;
+      return file.lastModifiedSync().millisecondsSinceEpoch;
     } catch (ex) {
       return null;
     }
@@ -55,8 +53,8 @@ class FileAPI {
             if (modificationDate != null) "modificationDate": modificationDate
           }, contentType: ContentType.binary.toString()),
           onSendProgress: (total, _) {
-            stream.add(total);
-          });
+        stream.add(total);
+      });
       return SavedFileState.fromJson(upload.data);
     } on DioException catch (ex, st) {
       stream.addError(ex, st);
@@ -78,8 +76,8 @@ class FileAPI {
 
   // The first future is for choosing/getting the path.
   // The returned future is the download future.
-  static Future<Response> downloadFile(SavedFileState savedFile,
-      String path) async {
+  static Future<Response> downloadFile(
+      SavedFileState savedFile, String path) async {
     final url = "/file/${savedFile.uuid.uuid}";
     final queryParams = fileGetParams();
     return client.download(url, path, queryParameters: queryParams);
@@ -90,8 +88,8 @@ class FileAPI {
         data: FormData.fromMap({"name": newName}));
   }
 
-  static Future<SavedFileState> getInfo(UuidValue fileId, TagOrder tagOrder,
-      bool tagsReversed) async {
+  static Future<SavedFileState> getInfo(
+      UuidValue fileId, TagOrder tagOrder, bool tagsReversed) async {
     final response = await client.get("/file/${fileId.uuid}/info",
         options: Options(headers: {
           "tagOrder": tagOrder.name,
@@ -102,8 +100,8 @@ class FileAPI {
 
   static CachedNetworkImage getThumbnail(SavedFileState savedFile) {
     assert(savedFile.thumbnail);
-    return CachedNetworkImage(imageUrl:
-    url("file/${savedFile.uuid}/thumb").toString(),
+    return CachedNetworkImage(
+      imageUrl: url("file/${savedFile.uuid}/thumb").toString(),
       httpHeaders: defaultHeaders(),
     );
   }
@@ -111,11 +109,12 @@ class FileAPI {
   static CachedNetworkImage getImage(SavedFileState savedFile) {
     assert(savedFile.mediaType == MediaType.IMAGE);
     final dimensions = savedFile.dimensions;
-    return CachedNetworkImage(imageUrl:
-    url("file/${savedFile.uuid}", queryParameters: fileGetParams())
-        .toString(),
+    return CachedNetworkImage(
+      imageUrl: url("file/${savedFile.uuid}", queryParameters: fileGetParams())
+          .toString(),
       width: dimensions?.width,
       height: dimensions?.height,
+      fadeInDuration: Duration.zero,
     );
   }
 
@@ -134,8 +133,8 @@ class FileAPI {
     return _getMedia(savedFile);
   }
 
-  static Future<void> _modifyTag(UuidValue fileId, UuidValue tagId,
-      bool add) async {
+  static Future<void> _modifyTag(
+      UuidValue fileId, UuidValue tagId, bool add) async {
     await client.patch("/file/${fileId.uuid}/${add ? "add" : "remove"}",
         data: FormData.fromMap({"tag": tagId.uuid}));
   }
@@ -154,7 +153,7 @@ class FileAPI {
 
   static Future<bool> checkExists(String fileName) async {
     final response =
-    await client.get("/file/exists/${Uri.encodeComponent(fileName)}");
+        await client.get("/file/exists/${Uri.encodeComponent(fileName)}");
     final exists = response.data["exists"];
     return exists;
   }
